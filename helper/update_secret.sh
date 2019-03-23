827fd66ce1c5cb44195d1cb2704aea76a3fcbdc7
andreadellacorte <andrea@dellacorte.me>
2019-03-23 18:52:44 +0000

    Update secret helper script

---
+++ helper/update_secret.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+travis login --com
+
+cp ~/.improbable/oauth2/* .
+
+tar cvf secrets.tar oauth2_access_token_spatial_improbable_cli_client_go oauth2_refresh_token
+
+travis encrypt-file secrets.tar -r andreadellacorte/space-game --com
