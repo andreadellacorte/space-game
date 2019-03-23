385538a2f1d55ff26c592d839dbf0b047f80ed02
andreadellacorte <andrea@dellacorte.me>
2019-03-23 11:13:53 +0000

    Modify secret

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -3,6 +3,6 @@
 set -e
 set -u
 
-openssl aes-256-cbc -K $encrypted_6aee264528f1_key -iv $encrypted_6aee264528f1_iv -in secret.enc -out secret -d
+openssl aes-256-cbc -K $encrypted_93597bdefda6_key -iv $encrypted_93597bdefda6_iv -in secret.enc -out secret -d
 mkdir -p ~/.improbable/oauth2
 mv ./secret ~/.improbable/oauth2/oauth2_refresh_token
