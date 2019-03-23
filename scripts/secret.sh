3832232bc8baae6374b8ef11d0302cbba3d5f69b
andreadellacorte <andrea@dellacorte.me>
2019-03-23 17:35:40 +0000

    Update secret

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -3,10 +3,11 @@
 set -e
 set -u
 
-openssl aes-256-cbc -K $encrypted_93597bdefda6_key -iv $encrypted_93597bdefda6_iv -in oauth2_refresh_token.enc -out oauth2_refresh_token -d
-openssl aes-256-cbc -K $encrypted_93597bdefda6_key -iv $encrypted_93597bdefda6_iv -in oauth2_access_token_spatial_improbable_cli_client_go.enc -out oauth2_access_token_spatial_improbable_cli_client_go -d
+openssl aes-256-cbc -K $encrypted_93597bdefda6_key -iv $encrypted_93597bdefda6_iv -in secrets.tar.enc -out secrets.tar -d
 
 mkdir -p ~/.improbable/oauth2
+
+tar xf secrets.tar
 
 mv ./oauth2_refresh_token ~/.improbable/oauth2/oauth2_refresh_token
 mv ./oauth2_access_token_spatial_improbable_cli_client_go ~/.improbable/oauth2/oauth2_access_token_spatial_improbable_cli_client_go
