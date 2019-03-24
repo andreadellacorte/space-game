1c51a6081276678785d559352db45b7b4d71989d
andreadellacorte <andrea@dellacorte.me>
2019-03-24 09:24:37 +0000

    Update helper function

--- helper/update_secret.sh
+++ helper/update_secret.sh
@@ -10,3 +10,7 @@ cp ~/.improbable/oauth2/* .
 tar cvf secrets.tar oauth2_access_token_spatial_improbable_cli_client_go oauth2_refresh_token
 
 travis encrypt-file secrets.tar -r andreadellacorte/space-game --com
+
+rm oauth2_access_token_spatial_improbable_cli_client_go
+rm oauth2_refresh_token
+rm secrets.tar
