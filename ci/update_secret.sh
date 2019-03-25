ba16fc0d8521438bb4c8f8b9de11cd3066c51856
andreadellacorte <andrea@dellacorte.me>
2019-03-25 09:14:44 +0000

    Update secret

--- ci/update_secret.sh
+++ ci/update_secret.sh
@@ -3,7 +3,9 @@
 set -e
 set -u
 
-travis login --com
+pushd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# travis login --com
 
 cp ~/.improbable/oauth2/* .
 
@@ -11,6 +13,10 @@ tar cvf secrets.tar oauth2_access_token_spatial_improbable_cli_client_go oauth2_
 
 travis encrypt-file secrets.tar -r andreadellacorte/space-game --com
 
+mv ./secrets.tar.enc ..
+
 rm oauth2_access_token_spatial_improbable_cli_client_go
 rm oauth2_refresh_token
 rm secrets.tar
+
+popd
