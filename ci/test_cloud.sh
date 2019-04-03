9a95a0655728610d1be612d3f91e154628b7038c
Andrea <andrea@dellacorte.me>
2019-04-04 01:09:58 +0200

    Feature/cloud launcher (#8)
    * Dev auth login for testing without BYOAuth
    
    * Adjust time message for nanobot level

--- ci/test_cloud.sh
+++ ci/test_cloud.sh
@@ -9,5 +9,6 @@ spatial alpha cloud upload -a $ASSEMBLY_NAME --force
 spatial alpha cloud launch -a $ASSEMBLY_NAME -d $DEPLOYMENT_NAME
 
 spatial project deployment tags add $DEPLOYMENT_NAME $DEPLOYMENT_TTL
+spatial project deployment tags add $DEPLOYMENT_NAME $DEV_LOGIN 
 
 popd
